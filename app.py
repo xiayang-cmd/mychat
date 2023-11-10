42c46575406fc753a2833a0c4ef04b36cabfe097
夏阳 <62921464+xiayang-cmd@users.noreply.github.com>
2023-11-10 18:38:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,13 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello():
+@app.route('/')  # 修改为根路径
+def index():  # 通常根路径的视图函数命名为index
     return 'Hello, World! This is your endpoint.'
+
+@app.route('/hello', methods=['GET'])  # 定义GET方法
+def hello():
+    return '呵呵呵呵'  # 返回指定字符串
 
 if __name__ == '__main__':
     app.run(port=5000)
